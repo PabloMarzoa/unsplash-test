123b8a13313b7fe15d1911d115b4c705418f10b4
Pablo <pablomarzoalopez@gmail.com>
2025-04-02 18:29:57 +0200

    add api connection

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {UnsplashRestService} from './services/rest/unsplash-rest.service';
+import {HttpClientModule} from '@angular/common/http';
+import {IPhoto} from './models/photo';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    imports: [
+        RouterOutlet,
+        HttpClientModule
+    ],
+    providers: [
+        UnsplashRestService
+    ],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'unsplash-test';
+    private unsplashRestService: UnsplashRestService = inject(UnsplashRestService);
+    photosCollection: IPhoto[] = [];
+
+    onGetAllPhotos(): void {
+        this.unsplashRestService.getPhotos().subscribe((res: IPhoto[]) => {
+            console.log('test-res', res);
+            this.photosCollection = res;
+        })
+    }
 }
---
+++ src/app/services/rest/unsplash-rest.service.ts
@@ -0,0 +1,26 @@
+import {inject, Injectable} from '@angular/core';
+import {catchError, Observable, throwError} from 'rxjs';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
+import {IPhoto} from '../../models/photo';
+
+@Injectable({providedIn: "root"})
+export class UnsplashRestService {
+  private accessKey = 'Client-ID d0IoCpSrR1SMMiKH0ZReNGy8CjNdTLfGcbJRsXDFDyc'; // Only for test projects
+  private httpService = inject(HttpClient);
+  public getPhotos(): Observable<IPhoto[]> {
+      return this.httpService.get<IPhoto[]>('https://api.unsplash.com/photos', {headers: this.setHttpHeaders()})
+          .pipe(catchError(error => this.handleError(error)));
+  }
+
+  private setHttpHeaders() {
+      let headers = new HttpHeaders({});
+      headers = headers.append('Authorization', this.accessKey);
+      return headers;
+  }
+
+  private handleError(error: any) {
+      alert('Check console to see the error: ' + error.status);
+      console.error('rest call error: ', error);
+      return throwError(error);
+  }
+}
